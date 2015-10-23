a25cc9c1739f83d5950929eeb3bc6e26580aab51
sslover <samjslover@gmail.com>
2015-10-22 21:14:13 -0400

    added working form and post route

---
+++ models/person.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// See http://mongoosejs.com/docs/schematypes.html
+
+var personSchema = new Schema({
+	name: String,
+	itpYear: Number,
+	interests: [String],
+	link: String,
+	imageUrl: String,
+	dateAdded : { type: Date, default: Date.now }
+})
+
+// export 'Person' model so we can interact with it in other files
+module.exports = mongoose.model('Person',personSchema);
