7e9dd1b20eff93c489c205319d82e60e9bca6c9f
sslover <samjslover@gmail.com>
2015-11-01 01:13:16 -0400

    adding images and s3 integration

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,21 @@ var mongoose = require('mongoose');
 var Person = require("../models/person.js");
 var Course = require("../models/course.js");
 
+// S3 File dependencies
+var AWS = require('aws-sdk');
+var awsBucketName = process.env.AWS_BUCKET_NAME;
+var s3Path = process.env.AWS_S3_PATH; // TODO - we shouldn't hard code the path, but get a temp URL dynamically using aws-sdk's getObject
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY
+});
+var s3 = new AWS.S3();
+
+// file processing dependencies
+var fs = require('fs');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+
 /**
  * GET '/'
  * Default home route. Just relays a success message back.
@@ -35,6 +50,12 @@ router.get('/', function(req, res) {
 router.get('/add-person', function(req,res){
 
   res.render('add.html')
+
+})
+
+router.get('/add-person-with-image', function(req,res){
+
+  res.render('add-with-image.html')
 
 })
 
@@ -183,6 +204,111 @@ router.post('/api/edit/:id', function(req,res){
   })
 
 })
+
+router.post('/api/create/image', multipartMiddleware, function(req,res){
+
+  console.log('the incoming data >> ' + JSON.stringify(req.body));
+  console.log('the incoming image file >> ' + JSON.stringify(req.files.image));
+
+  var personObj = {
+    name: req.body.name,
+    itpYear: req.body.itpYear,
+    interests: req.body.interests.split(','),
+    link: req.body.link,
+    slug : req.body.name.toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'-')
+  }
+
+  if (req.body.hasGlasses == 'yes') personObj['hasGlasses'] = true;
+  else personObj['hasGlasses'] = false;
+
+
+  // NOW, we need to deal with the image
+  // the contents of the image will come in req.files (not req.body)
+  var filename = req.files.image.name; // actual filename of file
+  var path = req.files.image.path; // will be put into a temp directory
+  var mimeType = req.files.image.type; // image/jpeg or actual mime type
+  
+  // create a cleaned file name to store in S3
+  // see cleanFileName function below
+  var cleanedFileName = cleanFileName(filename);
+
+  // We first need to open and read the uploaded image into a buffer
+  fs.readFile(path, function(err, file_buffer){
+
+    // reference to the Amazon S3 Bucket
+    var s3bucket = new AWS.S3({params: {Bucket: awsBucketName}});
+    
+    // Set the bucket object properties
+    // Key == filename
+    // Body == contents of file
+    // ACL == Should it be public? Private?
+    // ContentType == MimeType of file ie. image/jpeg.
+    var params = {
+      Key: cleanedFileName,
+      Body: file_buffer,
+      ACL: 'public-read',
+      ContentType: mimeType
+    };
+    
+    // Put the above Object in the Bucket
+    s3bucket.putObject(params, function(err, data) {
+      if (err) {
+        console.log(err)
+        return;
+      } else {
+        console.log("Successfully uploaded data to s3 bucket");
+
+        // now that we have the image
+        // we can add the s3 url our person object from above
+        personObj['imageUrl'] = s3Path + cleanedFileName;
+
+        // now, we can create our person instance
+        var person = new Person(personObj);
+
+        person.save(function(err,data){
+          if(err){
+            var error = {
+              status: "ERROR",
+              message: err
+            }
+            return res.json(err)
+          }
+
+          var jsonData = {
+            status: "OK",
+            person: data
+          }
+
+          return res.json(jsonData);        
+        })
+
+      }
+
+    }); // end of putObject function
+
+  });// end of read file
+
+})
+
+function cleanFileName (filename) {
+    
+    // cleans and generates new filename for example userID=abc123 and filename="My Pet Dog.jpg"
+    // will return "abc123_my_pet_dog.jpg"
+    var fileParts = filename.split(".");
+    
+    //get the file extension
+    var fileExtension = fileParts[fileParts.length-1]; //get last part of file
+    
+    //add time string to make filename a little more random
+    d = new Date();
+    timeStr = d.getTime();
+    
+    //name without extension
+    newFileName = fileParts[0];
+    
+    return newFilename = timeStr + "_" + fileParts[0].toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'_') + "." + fileExtension;
+    
+}
 
 router.get('/api/get', function(req,res){
 
